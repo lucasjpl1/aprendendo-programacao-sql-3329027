3e07fe30d27300941e3b0e05a3cdb447482f9084
Lucas jesus <126540649+lucasjpl1@users.noreply.github.com>
2025-04-10 15:12:27 +0000

    TESTES SQL

--- teste.sql
+++ teste.sql
@@ -1,3 +1,27 @@
 
 SELECT * from tracks;
 
+--structure 
+
+CREATE TABLE salario_classe (
+id integer primary key autoincrement,
+nivel text,
+salario integer
+);
+
+
+--Alterar tabela para casos especificos statement
+
+ALTER TABLE EMPLOYEES
+ADD ID_SALARIO INTEGER;
+
+--INSERIR DADOS 
+
+INSERT INTO salario_classe (id, SALARIO, nivel)
+VALUES
+(1, 9000, 'SENIOR'),
+(2, 2000, 'ESTAGIÁRIO'),
+(3, 7000, 'PLENO'),
+(4, 4000, 'JUNIOR');
+
+---
